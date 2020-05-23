d436be88a9852da1c9ac6d7af9969406901b63e6
Denner Torres <dennerstorres@icloud.com>
2020-05-23 16:36:47 -0400

    Finalizado Floating Browser

--- main.js
+++ main.js
@@ -1,43 +1,67 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const fs = require('fs');
+const path = require('path');
+const ini = require('ini');
+
+const config = ini.parse(fs.readFileSync(path.join('./SoluregWEB.ini'), 'utf-8'));
+
+let win = null
+let contents = null
 
 function createWindow () {
-  // Cria uma janela de navegação.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+
+  win = new BrowserWindow({
+    width: config.PARAMS.width,
+    height: config.PARAMS.height,
+    // transparent:true,
+    // frame:false,
+    autoHideMenuBar: true,
+    titleBarStyle: 'hidden',
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
   })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadURL(config.CONEXAO.url)
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  contents = win.webContents
+
 }
 
+function toggleDevTools() {
+  contents.toggleDevTools()
+}
+
+function createShortcuts() {
+  globalShortcut.register('CmdOrCtrl+J', toggleDevTools)
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Algumas APIs podem ser usadas somente depois que este evento ocorre.
-app.whenReady().then(createWindow)
+// Some APIs can only be used after this event occurs.
+app.whenReady()
+.then(createWindow)
+.then(createShortcuts)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // No macOS é comum para aplicativos e sua barra de menu 
-  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+app.on('activate', recreateWindow)
+
+
+function recreateWindow() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+}
 
 // In this file you can include the rest of your app's specific main process
-// code. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
+// code. You can also put them in separate files and require them here.
